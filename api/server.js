6ddb74a5bba97976974fe1fde705ef7f24798c3b
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-04-29 17:58:16 -0700

    data

--- api/server.js
+++ api/server.js
@@ -16,7 +16,7 @@ server.use(express.json());
 server.use("/api/auth", authRouter);
 server.use("/api/jokes", restrict, jokesRouter); // only logged-in users should have access!
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
--- api/server.test.js
+++ api/server.test.js
@@ -1,24 +1,14 @@
+const server = require("./server");
 const request = require("supertest");
 const db = require("../data/dbConfig");
-const server = require("../api/server");
+const bcrypt = require("bcryptjs");
 
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(true)
-})
+const password = "1234";
+const salt = 8;
 
-
-const user1 = {
-  id: 1,
-  username: "Reed",
-  password: "1234"
-}
-
-const user2 = {
-  id: 1,
-  username: "Pickles",
-  password: "kitty"
-}
+const generateHash = (password, salt) => {
+  return bcrypt.hashSync(password, salt);
+};
 
 beforeAll(async () => {
   await db.migrate.rollback();
@@ -27,40 +17,79 @@ beforeAll(async () => {
 
 beforeEach(async () => {
   await db("users").truncate();
+  await db("users").insert([
+    {
+      username: "reed",
+      password: generateHash(password, salt),
+    },
+    {
+      username: "john",
+      password: generateHash(password, salt),
+    },
+    {
+      username: "joe",
+      password: generateHash(password, salt),
+    },
+  ]);
 });
 
 afterAll(async () => {
   await db.destroy();
 });
 
-describe("POST /Register", () => {
-  test("Register a new User", async () => {
-    await request(server).post("/api/auth/register").send(user1);
-    const user = await db("users").where("username", "Reed").first();
-    expect(user).toMatchObject({ username: "Reed" });
-  });
-  test("Register responds with 'username taken'", async () => {
-    await request(server).post("/api/auth/register").send(user1);
-    const response = await request(server)
-      .post("/api/auth/register")
-      .send(user1);
-    expect(response.body.message).toMatch("username taken");
-  });
+test("[1] sanity", () => {
+  expect(true).not.toBe(false);
 });
 
-describe("POST /Login", () => {
-  test("Login successful", async () => {
+test("[2] make sure our environment is set correctly", () => {
+  expect(process.env.NODE_ENV).toBe("testing");
+});
+
+describe("HTTP API functions are working properly", () => {
+  test("[3] POST /api/auth/register [test 1] can properly register a new user", async () => {
     const res = await request(server)
-    .post("/api/auth/login")
-    .send(user1);
-    expect(res.status).toBe()
-    expect(res.body.message).toMatch("welcome, Reed");
+      .post("/api/auth/register")
+      .send({ username: "troy", password: "1234" });
+    expect(res.status).toBe(201);
+    expect(res.body).toMatchObject({ id: 4, username: "troy" });
   });
-  test("Login return error if credentials invalid", async () => {
+
+  test("[4] POST /api/auth/register [test 2] fails to create a user if username taken", async () => {
     const res = await request(server)
-    .post("/api/auth/login")
-    .send(user2);
-    expect(res.status).toBe(401)
-    expect(res.body.message).toMatch("invalid credentials");
+      .post("/api/auth/register")
+      .send({ username: "reed", password: "1234" });
+    expect(res.status).toBe(422);
+    expect(res.body).toHaveProperty("message", "username taken");
+  });
+
+  test("[5] POST /api/auth/login [test 1] can properly login a user", async () => {
+    const res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "john", password: "1234" });
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("message", "welcome, john");
+  });
+
+  test("[6] POST /api/auth/login [test 2] fails to login if password is incorrect", async () => {
+    const res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "john", password: "5678" });
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty("message", "invalid credentials");
+  });
+
+  test("[7] GET /api/jokes [test 1] fails to retrieve data without authorization jwt token header", async () => {
+    const res = await request(server).get("/api/jokes");
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty("message", "token required");
+  });
+
+  test("[8] GET /api/jokes [test 2] successfuly retrieves jokes with authorization header jwt token", async () => {
+    const login = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "bob", password: "1234" });
+    const res = await request(server).get("/api/jokes").set("Authorization", login.body.token);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(3);
   });
 });
